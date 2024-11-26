eccead3be518c123ab54ada68c41a30822d7fb1a
Alexander Kurz <alex.kurz95@gmail.com>
2024-11-26 17:32:08 +0100

    Add model and trainer to train_segformer

--- train_segformer.py
+++ train_segformer.py
@@ -4,11 +4,17 @@ from pathlib import Path
 from typing import Tuple
 
 import pandas as pd
+import torch
 from accelerate import Accelerator
 from PIL import Image
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
-from transformers import SegformerForSemanticSegmentation, SegformerImageProcessor
+from transformers import (
+    SegformerForSemanticSegmentation,
+    SegformerImageProcessor,
+    Trainer,
+    TrainingArguments,
+)
 
 
 class SegmentationDataset(Dataset):
@@ -47,24 +53,65 @@ class SegmentationDataset(Dataset):
         return img, mask
 
 
-def training_function(cfg):
+def training_function(cfg, training_args):
     dataset = SegmentationDataset(
         root_dir=Path(cfg["data_dir"]),
         # img_tfm=None,
         # mask_tfm=None,
     )
-    print(len(dataset))
+    # print(len(dataset))
     # print(dataset.df.head(20))
-    img, mask = dataset[0]
-    print("img.shape:", img.shape)
-    print("mask.shape:", mask.shape)
+    # img, mask = dataset[0]
+    # print("img.shape:", img.shape)
+    # print("mask.shape:", mask.shape)
+    id2label = dataset.id2label
+    label2id = {v: k for k, v in id2label.items()}
+
+    train_set, val_set = random_split(
+        dataset,
+        [0.9, 0.1],
+        generator=torch.Generator().manual_seed(cfg["seed"]),
+    )
+    # train_dataloader = DataLoader(train_set, batch_size=4, shuffle=True, num_workers=8)
+    # val_dataloader = DataLoader(val_set, batch_size=4, shuffle=False, num_workers=8)
+    # batch = next(iter(train_dataloader))
+    # imgs, masks = batch
+    # print(imgs.shape)
+
+    # Load model from hf hub and store it locally
+    # model = SegformerForSemanticSegmentation.from_pretrained(
+    #     "nvidia/mit-b0",
+    #     id2label=id2label,
+    #     label2id=label2id,
+    # )
+    # model.save_pretrained("./checkpoints/mit-b0")
+
+    # Load local model
+    model = SegformerForSemanticSegmentation.from_pretrained("./checkpoints/mit-b0/")
+    # print(model)
+
+    # TODO: We should use the SegformerImageProcessor for the datasets and convert the datasets to hf datasets
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_set,
+        eval_dataset=val_set,
+        # compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
 
 
 def main(
     data_dir: str = os.environ["HOME"] + "/Data/FacialAttributes/celebamask_hq",
+    seed: int = 42,
 ):
     cfg = locals()
-    training_function(cfg)
+    training_args = TrainingArguments(
+        output_dir="logs/",
+        num_train_epochs=2,
+    )
+    training_function(cfg, training_args)
 
 
 if __name__ == "__main__":
